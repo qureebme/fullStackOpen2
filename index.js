53739487a2f8ae3450fa9396b7d7d6d390a94c24
Qureeb Hameed <quareebs@yahoo.com>
2019-07-28 11:15:19 +0300

    added part 3.1: Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+app.get('/api/persons', function(req, res){
+    res.json(persons);
+})
+
+const port = 3001;
+app.listen(port, function(){
+    console.log(`server is running on port ${port}`);
+})
+
+const persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "qureeb hameed",
+      "number": "+2348026785418",
+      "id": 5
+    }
+  ]
