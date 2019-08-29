590627515b1cd1b94bddb0b2b0630932bd2440ad
Qureeb Hameed <quareebs@yahoo.com>
2019-08-29 16:03:35 +0300

    added 3.17*: Phonebook database, step4

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ app.use(express.static('build'))
 app.get('/api/persons', function(req, res){
   Person.find({})
   .then((data) => {res.json(data)})
-  .catch((err) => {console.log('Cannot get data:', err)})
+  .catch((err) => {
+    console.log('Cannot get data:')
+    next(err)
+  })
 })
 
-app.get('/info', function(req, res){
+app.get('/info', function(req, res, next){
   Person.find({}).countDocuments()
   .then((data) => {
     res.send(
@@ -29,11 +32,14 @@ app.get('/info', function(req, res){
       <p>${new Date()}</p>`
     )
   })
-  .catch((err) => {console.log('Cannot get info:', err)})
+  .catch((err) => {
+    console.log('Cannot get info:', err)
+    next(err)
+  })
   
 })
 
-app.get('/api/persons/:id', function(req, res){
+app.get('/api/persons/:id', function(req, res, next){
   Person.findById(req.params.id)
   .then((data) => {
     //let result = data.find((each) => each.id == req.params.id)
@@ -41,28 +47,52 @@ app.get('/api/persons/:id', function(req, res){
     if (data) res.send(data)
     else res.status(404).send("Person not found")
   })
-  .catch((err) => {console.log('Something went wrong:', err)})
+  .catch((err) => {
+    console.log('Something went wrong:', err);
+    next(err);
+  })
 })
 
-app.delete('/api/persons/:id', function(req, res){
+app.put('/api/persons/:id', function(req, res, next){
+  Person.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, doc){
+    if (err) next(err)
+    if (doc) res.json(doc)
+    else res.status(400).end('Document has been deleted')
+  })
+})
+
+app.delete('/api/persons/:id', function(req, res, next){
     Person.findOneAndDelete({_id: ObjectID(req.params.id)}, (err, result) => {
       if (err)  {
-        res.send('Something went wrong');
-        return console.log('Something went wrong:', err)
+        //res.send('Something went wrong');
+        console.log('Something went wrong:', err)
+        next(err)
     }
       res.json(result)
     })
 })
 
 app.post('/api/persons', function(req, res){
+
     let newPerson = new Person(req.body);
     newPerson.save()
     .then((data) => res.json(data))
     .catch((err)=> {
+
       console.log('Cannot save data:', err)
       res.status(500).send('Server Error')
     })
 })
+const unknownRouteHandler = function(req, res, next){
+  res.end('Unknown Route')
+}
+const errorhandler = function(err, req, res, next){
+  console.log('Error: ', err);
+  res.status(500).end('Something went wrong');
+}
+
+app.use(unknownRouteHandler)
+app.use(errorhandler)
 
 const port = process.env.PORT || PORT;
 app.listen(port, function(){
